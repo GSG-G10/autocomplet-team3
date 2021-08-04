44519b76e9ac72030e25b728c35fe61844158c22
muhammadharoun <hro19502001@gmail.com>
2021-08-04 11:26:08 +0300

    add search btn

--- public/main.js
+++ public/main.js
@@ -1,15 +1,17 @@
 const result = document.querySelector('.reslut');
 let inputValue = document.querySelector('.input-search');
-
+const saerchBtn = document.querySelector(".myBtn");
 function addListener(selector, action, callback) {
   document.querySelector(selector).addEventListener(action, callback);
 }
 // General  Function Api
 function api(url, callback) {
+  console.log('in api ')
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = () => {
     if (xhr.readyState === 4) {
-      if (xhr.status) {
+      console.log(xhr.status)
+      if (xhr.status == 200) {
         const response = JSON.parse(xhr.responseText);
         callback(response);
       }
@@ -19,11 +21,20 @@ function api(url, callback) {
   xhr.send();
 }
 function showResult(response) {
-  result.innerHTML = response.results.urls;
+
+  console.log(response.results[0].urls.raw)
+  result.innerHTML = response.results[0].urls.raw;
 }
-addListener('submit', (e) => {
+addListener('.myBtn','click', (e) => {
   e.preventDefault();
-  inputValue = e.target.querySelector('input').value;
-  const url = `https://api.unsplash.com/search/photos?query=${inputValue}&client_id=qp1xazQhIzra13wFLMNGz3ayhyy-ouNonVyzwcbtnLY`;
+  const url = `https://api.unsplash.com/search/photos?query=${inputValue.value}&client_id=qp1xazQhIzra13wFLMNGz3ayhyy-ouNonVyzwcbtnLY`;
   api(url, showResult);
+});
+
+
+inputValue.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.querySelector(".myBtn").click();
+  }
 });
